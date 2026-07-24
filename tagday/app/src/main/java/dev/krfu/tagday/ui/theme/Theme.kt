a56704c62cb4837c9b97c67bfe375f49ce9c714e
Krzysztof Furtak <kfurtak1024@gmail.com>
2026-07-25 00:36:46 +0200

    Small cleanup

--- tagday/app/src/main/java/dev/krfu/tagday/ui/theme/Theme.kt
+++ tagday/app/src/main/java/dev/krfu/tagday/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package dev.krfu.tagday.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
